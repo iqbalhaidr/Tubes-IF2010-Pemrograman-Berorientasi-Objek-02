09fe5c01534d34f0ec824a97a6fff984cf6045f1
jhotlann <19623263@std.stei.itb.ac.id>
2025-06-03 19:13:05 +0700

    feat: add User class

--- app/src/main/java/com/labpro/User.java
+++ app/src/main/java/com/labpro/User.java
@@ -1,25 +1,62 @@
 package com.labpro;
 
+import java.util.Objects;
+
 public class User {
     private String username;
     private String password;
     private boolean isAdmin;
 
     public User(String username, String password, boolean isAdmin) {
+        if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+            throw new IllegalArgumentException("Username dan password tidak bisa null atau kosong.");
+        }
         this.username = username;
         this.password = password;
         this.isAdmin = isAdmin;
     }
 
     public String getUsername() {
-        return username;
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        if (username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("Username tidak bisa null.");
+        }
+        this.username = username;
     }
 
     public String getPassword() {
-        return password;
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password tidak bisa null.");
+        }
+        this.password = password;
     }
 
     public boolean isAdmin() {
-        return isAdmin;
+        return this.isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.isAdmin = admin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User u = (User) o;
+        return (this.username != null && this.username.equals(u.username)) && this.isAdmin == u.isAdmin;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, isAdmin);
     }
 }
