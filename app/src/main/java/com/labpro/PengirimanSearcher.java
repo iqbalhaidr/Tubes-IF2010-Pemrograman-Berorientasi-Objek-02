a9ec27d4fea91eeab361fa509eb6efec56d21f47
Julius <juliusarthur2005@gmail.com>
2025-06-04 15:47:44 +0700

    feat: Pengiriman Searcher class

---
+++ app/src/main/java/com/labpro/PengirimanSearcher.java
@@ -0,0 +1,32 @@
+package com.labpro;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+public class PengirimanSearcher<T extends Pengiriman> {
+    private ArrayList<T> listpengiriman;
+    private SearchCriteria searchCriteria;
+
+    public PengirimanSearcher(ArrayList<T> pengiriman) {
+        this.listpengiriman = pengiriman;
+    }
+
+    public Collection<? extends T> search(SearchCriteria searchCriteria){
+        Collection<T> hasilpengiriman = new ArrayList<T>();
+        for (T pengiriman : listpengiriman) {
+            Collection<String> allDetailString;
+            Map<String, String> allDetail = pengiriman.getDetail();
+            allDetailString = allDetail.values();
+            for (String detail: allDetailString) {
+                if (detail.contains(searchCriteria.getQuery())){
+                    hasilpengiriman.add(pengiriman);
+                    break;
+                }
+            }
+        }
+
+        return hasilpengiriman;
+    }
+}
