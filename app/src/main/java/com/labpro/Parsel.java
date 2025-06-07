c6b23b844f3a748d28e3a5d7b1b965ff31353774
AgungLucker <iniabi838@gmail.com>
2025-06-07 14:50:14 +0700

    fix: parsel

--- app/src/main/java/com/labpro/Parsel.java
+++ app/src/main/java/com/labpro/Parsel.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-class Parsel implements Data {
+public class Parsel implements Data {
     private int ID;
     private ParselStatus status;
     private int[] dimensi = new int[3];
